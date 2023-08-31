e3229543b54641928c765ad4982c5b8e32a47b3c
Ananda Das <65402344+Ananda1405@users.noreply.github.com>
2023-08-31 21:49:54 +0600

    category button added

--- js/index.js
+++ js/index.js
@@ -1,1 +1,19 @@
-console.log('object');
+const handleCategory = async() =>{
+    const res = await fetch("https://openapi.programming-hero.com/api/videos/categories");
+    const data = await res.json();
+
+    const tabContainer = document.getElementById('tab-container');
+
+    data.data.forEach((category)  =>{
+        const tabContainerDiv = document.createElement('div');
+        tabContainerDiv.innerHTML = `
+        <a class="tab">${category.category}</a> 
+        `;
+
+        tabContainer.appendChild(tabContainerDiv);
+    });
+
+    console.log(data.data);
+}
+
+handleCategory();
